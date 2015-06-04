9abc8bd7ba30263481bfe4b3fc5fb4875c157a85
jlramirez <joel.ramirez001@gmail.com>
2015-06-04 15:48:06 -0400

    Main.js function

--- public/map/js/main.js
+++ public/map/js/main.js
@@ -16,7 +16,29 @@ var mapper = require('./lib/map');
 window.$ = require('jquery');
 var _ = require('lodash');
 
-$(document).ready(function(){
+
+$(document).ready(function()
+{
+
+    //Mapping
 	var map = new mapper('map');
-	map.query({"genus":"acer"}, ['1976-1-10','1999'], 5);
+	map.query({"genus":"accer"},5);
+
+
+
+    $('#mapping').submit(function(){
+
+        var genus = document.getElementById("genus").value;
+        var family = document.getElementById("family").value;
+        var species = document.getElementById("species").value;
+        var date1 = document.getElementById("date1").value;
+        var date2 = document.getElementById("date2").value;
+
+        var map = new mapper('map');
+        map.query({"genus":genus, "family":family, "species":species}, [date1,date2],5);
+
+    });
+
+
+
 })
